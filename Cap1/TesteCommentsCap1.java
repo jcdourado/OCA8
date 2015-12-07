6f328fa17dca012a424caa4284283b5fe5ff828d
jcdourado <jcd17071997@hotmail.com>
2015-12-07 13:32:33 -0200

    Acabado

---
+++ Cap1/TesteCommentsCap1.java
@@ -0,0 +1,13 @@
+public class TesteCommentsCap1{
+	public static void main(String args[]){
+		/*
+		 * /* teste */ //aqui ele não permite pois o compilador ve isto(/* */ */)
+		 */  //                                                               ^(erro)
+
+		// o método main só pode ter aquela assinatura "public static void main(Aqui poder ser(String[]
+		// args, String args[] ou String... args), esses são os argumentos passados
+
+		// cada arquivo de classe pode ter mais de uma classe, que só pode ser, package ou publica
+		// não necessita ter uma public obrigatoriamente, mas, se for ter, apenas uma pode.
+	}
+}
