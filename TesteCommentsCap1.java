7ee79fffebd6f70a3ae988a3289445e2b361f5eb
jcdourado <jcd17071997@hotmail.com>
2015-12-07 12:53:57 -0200

    Comments Cap 1
    Commets Capitulo 1

---
+++ TesteCommentsCap1.java
@@ -0,0 +1,7 @@
+public class TesteCommentsCap1{
+	public static void main(String args[]){
+		/*
+		 * /* teste */ //aqui ele não permite pois o compilador ve isto(/* */ */)
+		 */  //                                                               ^(erro)
+	}
+}
