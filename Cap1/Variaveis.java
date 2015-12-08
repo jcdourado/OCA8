d464b876356f5c5e6f704930fc96624b4c96e96d
jcdourado <jcd17071997@hotmail.com>
2015-12-08 13:34:00 -0200

    Variaveis

---
+++ Cap1/Variaveis.java
@@ -0,0 +1,18 @@
+public class Variavéis{
+	// variavel é um pedaço da memória que guarda dados, ela possui um nome e um tipo
+
+	// varias delas podem ser declaradas e inicializadas na mesma linha, mas devem ser 
+	// do mesmo tipo!
+
+	// NÃO se pode fazer isto: double d1, double d2 pois nao pode se repetir o identificador
+
+	// NEM ISTO: int i1; i2; <- ERRO!
+
+	// o nome de identificadores (classes, variaveis, metodos e campos) possui 3 regras:
+		// O nome precisa começar com UMA LETRA ou com os simbolos $ ou _
+		// Os caracteres seguintes podem ser NUMEROS, ou letras, apenas, e os simbolos acima
+		// Não podem ser keywords, mas, pode se usar o recurso do case sensitive
+
+	
+				
+}
