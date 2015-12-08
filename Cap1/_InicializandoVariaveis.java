ffad665204b1905349473e89918f176d4c91ee01
jcdourado <jcd17071997@hotmail.com>
2015-12-08 13:57:03 -0200

    InicializandoVariavies

---
+++ Cap1/_InicializandoVariaveis.java
@@ -0,0 +1,10 @@
+public class _InicializandoVariaveis{
+	// Variaveis locais precisam ser inicializadas antes de serem usadas, elas não tem um valor
+	// padrão, e o compilador não permite que seja lido seu valor até que seja inicializado
+
+	// Variaveis de instancia (campo) ou de classe (estatica)  são inicializadas com estes valores
+	// por padrão:  primitivos (false, 0.0 ou 0) de referencia (null).
+
+		
+
+}
