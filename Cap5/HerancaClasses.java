e07afd72d9121d129c1f8a71978368707238c7a7
jcdourado <jcd17071997@hotmail.com>
2015-12-16 11:03:15 -0200

    Commit inicial capitulo 5  - Heranca de clases

---
+++ Cap5/HerancaClasses.java
@@ -0,0 +1,17 @@
+public class HerancaClasses{
+	// Assim como o this, o super deve ser o primeiro do construtor
+	// O java chama o Construtor da mae na primeira linha
+	// O this pode ser usado pra chamar coisas da Super-Classe
+	
+	// Overriding:
+		// Tem que ter a mesma assinatura da super classe
+		// Tem ter a mesma acessibilidade, ou ser mais acessivel
+		// Nao pode lançar excecoes que sao novas 
+		// Pode retirar as excecoes!!!!
+		// Se o metodo retorna um valor, este deve ser 
+			// uma subclasse ou uma classe deste
+	
+	// Estaticos:
+		// Se tem static na da mae, tem que ter na filha
+
+}
