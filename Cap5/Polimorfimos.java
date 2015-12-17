8c82767b8f8609134ee870408d037d3c61ae57a5
jcdourado <jcd17071997@hotmail.com>
2015-12-17 15:20:11 -0200

    Polimorfismo

---
+++ Cap5/Polimorfimos.java
@@ -0,0 +1,10 @@
+public class Polimorfismo{
+	// Objetos superiores nao conhecem as partes mais especificas dos interiores
+	// casting explicito!!
+	// uma excecao pode ser lançada caso o objeto nao seja uma instancia da 
+		// classe atual
+
+	
+
+
+}
