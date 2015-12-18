ffe2c275cbac15ee461b2dc030604ca603b04672
jcdourado <jcd17071997@hotmail.com>
2015-12-18 15:23:57 -0200

    Exceptions

---
+++ Cap6/Exceptions.java
@@ -0,0 +1,23 @@
+public class Exceptions{
+	// Object -> Throwable -> Exception, Error e RuntimeExcpetion	
+	
+	// RuntimeExceptions == unchecked exceptions
+	// Exception e todas suas subclasses, exceto a de cima == checked exceptions
+	// Erros == estao fora do nosso controle, nao se pode fazer nda
+
+	// Precisa dos {}
+	// try nao precisa de um catch, mas se nao o tiver, tem q ter o finally
+	
+	// O java executa na ordem em que ele achar a exception
+		// e se uma superclasse estiver acima de uma classe, ele nao compila
+		
+	// Metodos sobrescritos podem dar throw em RuntimeException!!
+
+	// Checked Exceptions:
+		// IOExceptions -> FineNotFoundException
+	// Errors Exceptions:
+		// ExceptionInInitializerError : Erro na inicializacao de classes
+		// StackOverflowError: Erro de recursao infinita
+		// NoClassDefFoundError: nao cai no exame: nao encontra a classe
+
+}
